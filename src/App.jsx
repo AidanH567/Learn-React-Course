9002ea36f810771734ac898a8f3c706bb1a290e0
AidanH567 <herstikaidan@gmail.com>
2025-10-07 18:44:48 +0200

    continueing course

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,15 @@ function App() {
   return (
     <>
      <h1>Hello React!</h1>
-     
+     <img src="react-logo.png" width="40px"/>
+     <h1>Fun Facts About React</h1>
+    <ul>
+    <li>React is very cool</li>
+    <li>React is made by meta</li>
+    <li>React was made in 2013</li>
+    <li>I like using React</li>
+    <li>React is very popular</li>
+    </ul>
     <MyAwesomeh1/>
     </>
   )
