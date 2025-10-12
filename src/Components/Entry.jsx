9d2f625c9e46443bfbde718e85f41f8501d978e3
AidanH567 <herstikaidan@gmail.com>
2025-10-12 21:54:37 +0200

    added entry component

---
+++ src/Components/Entry.jsx
@@ -0,0 +1,16 @@
+function Entry() {
+    return (
+        <div className="entry-box">
+            <div className="image-div">
+                <img src="" alt="" />
+            </div>
+            
+            <div className="info-div"></div>
+            <h1 className="info-h1-country"></h1>
+            <p> date</p>
+            <p> Infomation</p>
+        </div>
+    )
+}
+
+export default Entry
