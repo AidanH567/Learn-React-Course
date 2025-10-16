296625af4273323e74b31cbb873f1d901a33c059
AidanH567 <herstikaidan@gmail.com>
2025-10-16 18:42:54 +0200

    changes to travel journal

--- src/Components/Entry.jsx
+++ src/Components/Entry.jsx
@@ -14,8 +14,8 @@ function Entry() {
                     <span>View on Google Maps</span>
                     </div>
             <h1 className="info-h1-country">Mount Fuji</h1>
-            <p>12 Jan, 2021 - 24 Jan, 2021</p>
-            <p>Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
+            <p className='info-date'>12 Jan, 2021 - 24 Jan, 2021</p>
+            <p className='info-summary'>Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
         </div></div>
     )
 }
