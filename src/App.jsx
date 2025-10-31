16c0fb83cb3c5570b7f4d986c426be39a340bd17
AidanH567 <herstikaidan@gmail.com>
2025-10-31 12:17:58 +0100

    starting tenyies game

--- src/App.jsx
+++ src/App.jsx
@@ -55,16 +55,18 @@ const [starWarsData, setStarWarsData] = React.useState({})
         .then(res => res.json())
         .then( data => setStarWarsData(data) )
     },[])
-
-  return (
-    <>
-    <Header3/>
+    {/* <Header3/>
     <Main/>
     <div>
             <h2>The count is {count}</h2>
             <button onClick={() => setCount(prevCount => prevCount + 1)}>Add</button>
             {starWarsData.name && <pre>{JSON.stringify(starWarsData, null, 2)}</pre>}
-        </div>
+        </div> */}
+
+  return (
+    <>
+    
+        <main className="main-box"> hello</main>
     
 </>
   )
