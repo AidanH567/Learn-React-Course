25e9821f0e480b965590c9326f711df8d4b627cd
AidanH567 <herstikaidan@gmail.com>
2025-11-08 19:05:50 +0100

    expandind out on header component

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,13 +1,27 @@
 function Header() {
     return (
         <header className="header">
-            <img src="react-logo.png" className="img" alt="React logo" />
-            <nav>
-            <ul className="list">
-            <li className="nav-list-item">Pricing</li>
-            <li className="nav-list-item">About</li>
-            <li className="nav-list-item">Contact</li>
-            </ul>
+            <div className="header-left">
+                <img 
+                    src="react-logo.png" 
+                    className="logo" 
+                    alt="React logo" 
+                />
+                <h1 className="site-title">ReactFacts</h1>
+            </div>
+
+            <nav className="nav">
+                <ul className="nav-list">
+                    <li className="nav-item">
+                        <a href="#pricing" className="nav-link">Pricing</a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="#about" className="nav-link">About</a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="#contact" className="nav-link">Contact</a>
+                    </li>
+                </ul>
             </nav>
         </header>
     )
